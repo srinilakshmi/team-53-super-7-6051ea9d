240131ec0b82bd78408306c511bd5d2a8855abef
Ben Prejean <benprejean@live.com>
2023-09-21 15:46:58 +0000

    Added initial Position class

---
+++ LevelUpGame.Tests/levelup/PositionTest.cs
@@ -0,0 +1,31 @@
+using NUnit.Framework;
+using levelup;
+
+namespace levelup
+{
+    [TestFixture]
+    public class PositionTest
+    {
+        private Position? testObj;
+
+        [SetUp]
+        public void SetUp()
+        {
+            testObj = new Position();
+        }
+
+        [Test]
+        public void IsPositionInitialized()
+        {
+            Assert.AreEqual(testObj.X, -1);
+            Assert.AreEqual(testObj.Y, -1);
+        }
+/*
+        [Test]
+        public void IsPositionWithCoordinatesInitialized()
+        {
+            Position testPosition = new Position(0,0);
+            Assert.IsNotNull(testObj);
+        } */
+    }
+}
---
+++ LevelUpGame/levelup/Position.cs
@@ -0,0 +1,19 @@
+namespace levelup 
+{
+
+    public class Position
+    {
+
+        public Position()
+        {
+            X = -1;
+            Y = -1;
+        }
+
+        public int X
+        { get;}
+        public int Y
+        { get;}
+    }
+
+}
